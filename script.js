fb3aad8627260aa82372ef03a11af90d54013afd
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-20 01:56:09 +0800

    fix bugs for round looping & added round counters & fix logic for when user input wrong choice & fix bug when user input is null or wrong

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Declare the players score variables = DONE
-// Create two new variables named humanScore and computerScore in the global scope = DONE
-// Initialize those variables with the value of 0 = DONE
+// to track score
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 0;
+let roundCounter = 1;
 
-// Create a new function named getComputerChoice
+// function to randomly generate computer choice
 function getComputerChoice(){
-// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”
        let computerChoice = Math.floor(Math.random() * 3) + 1;
 
     switch (computerChoice){
@@ -22,69 +21,68 @@ function getComputerChoice(){
     }
     return computerChoice;
 }
-// Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step
 
-
-// Write the logic to get the human choice
-// Create a new function named getHumanChoice
+// function to get human/user input/choice
 function getHumanChoice(){
-// Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs
     let humanChoice = prompt('Enter your choice: \n Rock \n Paper \n Scissors');
+    while (humanChoice == null){
+        humanChoice = prompt('Enter your choice: \n Rock \n Paper \n Scissors');
+    }
     return humanChoice;
+    
 }
-// Test what your function returns by using console.log
 
-
-// Write the logic to play a single round
-// Create a new function named playRound
-// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments
+// function to compare the human choice and computer choice
 function playRound(humanChoice, computerChoice){
-// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations
+
+// make the humanChoice & computerChoice value lowercase so it don't have to be case sensitive 
     humanChoice = humanChoice.toLowerCase();
     computerChoice = computerChoice.toLowerCase();
-// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”
+
     if (humanChoice === "rock" && computerChoice === "paper"){
         alert("your choice is: " + humanChoice + "\nvs" + "\ncomputer choice is: " + computerChoice);
         alert("You Lose!");
         console.log("You Lose!");
         computerScore++;
-    }else if (humanChoice === "paper" && computerChoice === "scissors"){
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors"){
         alert("your choice is: " + humanChoice + "\nvs" + "\ncomputer choice is: " + computerChoice);
         alert("You Lose!");
         console.log("You Lose!");
         computerScore++;
-    }else if (humanChoice === "scissors" && computerChoice === "rock"){
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock"){
         alert("your choice is: " + humanChoice + "\nvs" + "\ncomputer choice is: " + computerChoice);
         alert("You Lose!");
         console.log("You Lose!");
         computerScore++;
-    }else if (humanChoice === computerChoice) {
+    }
+    else if (humanChoice === computerChoice) {
         alert("your choice is: " + humanChoice + "\nvs" + "\ncomputer choice is: " + computerChoice);
         alert("It's a Draw!");
         console.log("It's a Draw");
-    }else {
+    }
+    else if (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors"){
+        alert("Invalid choice! \n Please input your choice again");
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    else {
         alert("your choice is: " + humanChoice + "\nvs" + "\ncomputer choice is: " + computerChoice);
         alert("You Win!");
         console.log("You Win!");
         humanScore++;
     }
-// Increment the humanScore or computerScore variable based on the round winner. == DONE
+    
+    
 }
 
-
-// Write the logic to play the entire game
-// Create a new function named playGame
+// play the game and loop for i amount of times
 function playGame(){
-
-for (let i = 0; i < 5; i++){
-    playRound(getHumanChoice(), getComputerChoice());
-    console.log("Play Score: " + humanScore);
-    console.log("Play Score: " + computerScore);
+    rounds = Number(prompt("How many rounds do you want to play?"));
+    for (roundCounter; roundCounter <= rounds; roundCounter++){
+        // invoke playRound function and use getHumanChoice and getComputerChoice as parameters for the comparison
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log("Play Score: " + humanScore);
+        console.log("Play Score: " + computerScore);
+    }
 }
-}
-
-// Move your playRound function and score variables so that they’re declared inside of the new playGame function
-
-// Play 5 rounds by calling playRound 5 times.
-
-// 
