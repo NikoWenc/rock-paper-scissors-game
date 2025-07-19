d65b94bf47bed2b93fb7f2d24838f7624df28f84
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-20 00:01:14 +0800

    make some changes for player choice

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Declare the players score variables = DONE
+// Create two new variables named humanScore and computerScore in the global scope = DONE
+// Initialize those variables with the value of 0 = DONE
+let humanScore = 0;
+let computerScore = 0;
+
 // Create a new function named getComputerChoice
 function getComputerChoice(){
 // Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”
@@ -24,37 +30,21 @@ console.log(getComputerChoice());
 // Create a new function named getHumanChoice
 function getHumanChoice(){
 // Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs
-    let humanChoice = Number(prompt('Enter your choice \n "1" for Rock \n "2" for Paper \n "3" for Scissors'));
-
-    switch (humanChoice){
-        case 1:
-            humanChoice = "Rock";
-            break;
-        case 2:
-            humanChoice = "Paper";
-            break;
-        case 3:
-            humanChoice = "Scissors";
-            break;
-    }
+    let humanChoice = prompt('Enter your choice: \n Rock \n Paper \n Scissors');
     return humanChoice;
 }
 // Test what your function returns by using console.log
 console.log(getHumanChoice());
 
-
-// Declare the players score variables
-// Create two new variables named humanScore and computerScore in the global scope
-
-// Initialize those variables with the value of 0
-
-
 // Write the logic to play a single round
 // Create a new function named playRound
-
+function playRound(humanChoice, computerChoice){
 // Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments
 
 // Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations
+
+
+}
 
 // Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”
 
